f1a117348681753387aee2f7968d359d3229d140
Priya Dharsini P <dharsini15306@gmail.com>
2024-11-21 13:53:19 +0530

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // If you want all routes to be dynamic by default
+  dynamicDependencies: true,
   experimental: {
-    instrumentationHook: false
+    serverActions: true,
+    // Disable static page generation if you're heavily using dynamic routes
+    staticPageGenerationTimeout: 60
   }
 }
 
--- src/app/api/counts/route.js
+++ src/app/api/counts/route.js
@@ -5,7 +5,7 @@ import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-
+export const dynamic = 'force-dynamic'
 export async function GET() {
   try {
     const session = await getServerSession(authOptions);
