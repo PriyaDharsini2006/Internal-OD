a506873e5927138b7ccc53b507191d1a4b747d23
MukeshG7172 <mukeshrjpm1@gmail.com>
2024-12-05 20:09:30 +0530

    Copyrights Color

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,41 +1,4 @@
 
-// "use client";
-// import localFont from "next/font/local";
-// import "./globals.css";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
-
-
-// import { SessionProvider } from "next-auth/react";
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body>
-//         <SessionProvider>{children}</SessionProvider>
-//       </body>
-//       <footer className="bg-black shadow-md mt-auto">
-//         <div className="container mx-auto px-4 py-4">
-//           <div className="flex items-center justify-center space-x-2 text-gray-600 font-grotesk text-[#00f5d0]">
-//             <p className="text-sm font-grotesk">
-//               {new Date().getFullYear()} Copyrights@Hackerz.
-//             </p>
-//           </div>
-//         </div>
-//       </footer>
-//     </html>
-//   );
-// }
 "use client";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -60,7 +23,7 @@ export default function RootLayout({ children }) {
         <SessionProvider>{children}</SessionProvider>
         <footer className="bg-black shadow-md mt-auto">
           <div className="container mx-auto px-4 py-4">
-            <div className="flex items-center justify-center space-x-2 text-gray-600 font-grotesk text-[#00f5d0]">
+            <div className="flex items-center justify-center space-x-2 text-white font-grotesk text-[#00f5d0]">
               <p className="text-sm font-grotesk">
                 {new Date().getFullYear()} Copyrights@Hackerz.
               </p>
