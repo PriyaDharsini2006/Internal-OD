d651b30302e61c84a57675797175ef9ee5d0e24e
Priya Dharsini P <dharsini15306@gmail.com>
2024-12-07 12:45:16 +0530

    error check

--- src/app/api/user/[user_id]/route.js
+++ src/app/api/user/[user_id]/route.js
@@ -2,7 +2,6 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../auth/[...nextauth]/route';
 const prisma = new PrismaClient();
 
 export async function GET(request, { params }) {
