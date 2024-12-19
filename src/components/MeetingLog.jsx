4b6785ecd57eeb67e67f4812988dd21b2927c4cf
MukeshG7172 <mukeshrjpm1@gmail.com>
2024-12-19 11:08:01 +0530

    Logo update

--- src/components/MeetingLog.jsx
+++ src/components/MeetingLog.jsx
@@ -135,6 +135,8 @@ const MeetingLog = ({ meetings, setMeetings, fetchMeetings }) => {
       return student;
     }).filter(student => student);
 
+    const logoBase64 = process.env.logoBase64;
+    const cit = process.env.cit;
 
 
     // Base64 encoded logo (replace this with your actual logo's base64 string)
